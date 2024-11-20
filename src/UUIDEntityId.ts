4e3490243e219c431adcd7518bc1c0d0b4fca2e8
dimitri casier <dimitri.casier@howest.be>
2024-11-20 13:40:44 +0100

    refactor UUIDEntityId: remove generate method, refactor to  optional id to contructor and add type check in equals

--- src/UUIDEntityId.ts
+++ src/UUIDEntityId.ts
@@ -5,8 +5,8 @@
  * https://opensource.org/licenses/MIT
  */
 
-import type { EntityId } from '@domaincrafters/domain/mod.ts';
 import { UUID } from '@domaincrafters/std';
+import type { EntityId } from '@domaincrafters/domain/EntityId.ts';
 
 /**
  * Represents a UUID-based Entity Identifier in the context of Domain Driven Development.
@@ -19,12 +19,11 @@ import { UUID } from '@domaincrafters/std';
  * // Example of extending UUIDEntityId for a specific domain entity
  * class UserId extends UUIDEntityId {}
  *
- * // Generating a new UUID for a UserId
- * const newUserId = UserId.generate();
- * const userId = new UserId(newUserId);
- *
+ * // Generating a new UserID based on UUID
+ * const userId = UserId.create();
+
  * // Comparing two UserIds for equality
- * const anotherUserId = new UserId(UserId.generate());
+ * const anotherUserId = UserId.create();
  * const isEqual = userId.equals(anotherUserId);
  * console.log(`Are User IDs equal? ${isEqual}`);
  *
@@ -35,35 +34,27 @@ import { UUID } from '@domaincrafters/std';
  * console.log(`UUID Value: ${userId.value}`);
  * ```
  */
+
 export abstract class UUIDEntityId implements EntityId {
-    private readonly _id: UUID;
+    protected readonly _id: UUID;
 
     /**
      * Creates an instance of UUIDEntityId.
      *
-     * @param id - The UUID string to initialize the entity ID.
+     * @param id - The optional UUID string value to use as the entity identifier. If not provided, a new UUID will be generated.
      */
-    protected constructor(id: string) {
-        this._id = UUID.parse(id);
-    }
-
-    /**
-     * Generates a new UUID string for the entity ID.
-     *
-     * @returns A newly generated UUID string.
-     */
-    static generate(): string {
-        return UUID.create().value;
+    protected constructor(id?: string) {
+        this._id = (!id) ? UUID.create() : UUID.parse(id);
     }
 
     /**
      * Compares this entity ID with another to determine equality.
      *
-     * @param other - The other EntityId to compare with.
-     * @returns `true` if both EntityIds have the same value, otherwise `false`.
+     * @param other - The other UUIDEntityId to compare with.
+     * @returns `true` if both UUIDEntityIds have the same value and type, otherwise `false`.
      */
-    equals(other: EntityId): boolean {
-        return this.value === other.value;
+    equals(other: UUIDEntityId): boolean {
+        return this._id.equals(other._id) && this.constructor === other.constructor;
     }
 
     /**
